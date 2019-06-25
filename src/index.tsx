f2618f33435fc0a50dcdc465199b4a6d509f444b
Alexandre Fontoura <xesfnet@gmail.com>
2019-06-25 22:24:46 +0100

    Project re-structured. Electron app added pointing to React app.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import './ui/index.css';
+import App from './ui/App';
+import * as serviceWorker from './ui/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
