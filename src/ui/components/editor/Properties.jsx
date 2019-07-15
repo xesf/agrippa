4456353695cf3064f02796c88497d53cd5fa1339
Alexandre Fontoura <xesfnet@gmail.com>
2019-07-15 22:05:34 +0100

    Creating properties input fields.

--- src/ui/components/editor/Properties.jsx
+++ src/ui/components/editor/Properties.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { Input } from 'semantic-ui-react';
+
+const skipKeys = ['selected'];
+
+const inputStyle = {
+    width: '100%',
+    paddingBottom: '0.5em',
+};
 
 const Properties = (props) => {
+    const { node } = props;
     return (
         <div className="layout-border layout-properties">
-            Properties
+            {node && Object.keys(node).map((key, i) => {
+                    if (skipKeys.includes(key)) {
+                        return null;
+                    }
+                    return (
+                        <Input
+                            size='mini'
+                            label={key}
+                            placeholder={key}
+                            defaultValue={node[key]}
+                            style={inputStyle} />
+                    )
+                }
+            )}
         </div>
     );
 };
 
-export default Properties;
+const mapStateToProps = (state) => ({
+    node: state.properties.node,
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+        // setNodeProperties: (node) => dispatch(setProperties(node)),
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Properties);
