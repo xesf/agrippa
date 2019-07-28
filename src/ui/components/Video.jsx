7a5737c7960b8fa138db403a0143f7f5c2980d96
Alexandre Fontoura <xesfnet@gmail.com>
2019-07-28 09:40:15 +0100

    Link to next video node. Double click to Skip. Click to Pause/Play.

--- src/ui/components/Video.jsx
+++ src/ui/components/Video.jsx
@@ -4,6 +4,16 @@ export default class Video extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.videoRef = React.createRef();
+    }
+
+    togglePlay() {
+        if (this.videoRef.current.paused) {
+            this.videoRef.current.play();
+        } else {
+            this.videoRef.current.pause();
+        }
+        return !this.videoRef.current.paused;
     }
 
     render() {
@@ -11,6 +21,7 @@ export default class Video extends React.Component {
         return (
             <div className="video-preview-container">
                 <video
+                    ref={this.videoRef}
                     {...rest}
                 >
                     {children}
--- src/ui/components/editor/NodePreview.jsx
+++ src/ui/components/editor/NodePreview.jsx
@@ -12,86 +12,90 @@ class NodePreview extends React.Component {
         this.player = null;
     }
 
-    // componentDidMount() {
-    //     this.player.subscribeToStateChange(this.handleStateChange.bind(this));
-    // }
+    linkNode() {
+        const { links, node, nodes, setNodeProperties } = this.props;
+        const link = links.find(l => l.source === node.id);
+        if (link) {
+            const targetNode = nodes.find(n => n.id === link.target);
+            setNodeProperties(targetNode);
+        }
+    }
 
-    // handleStateChange(state) {
-    //     // copy player state to this component's state
-    //     this.setState({
-    //         ended: state.ended
-    //     }, () => {
-    //         const { links, node, nodes, setNodeProperties } = this.props;
-    //         if (this.state.ended) {
-    //             const link = links.find(l => l.source === node.id);
-    //             const targetNode = nodes.find(n => n.id === link.target);
-    //             setNodeProperties(targetNode);
-    //         }
-    //     });
-    // }
-
-    handleOnClick(d) {
+    handleDecisionOnClick(d) {
         const node = this.props.nodes.find(n => n.id === d.option);
         this.props.setNodeProperties(node);
+    }
+
+    handleOnClick() {
+        this.player.togglePlay();
+    }
+
+    handleOnDoubleClick() {
+        this.linkNode();
+    }
+
+    handleOnEnded() {
+        this.linkNode();
     }
 
     render() {
         const { node } = this.props;
         return (
             <div className="screen-container">
-                <div>
-                    <Video
-                        ref={(player) => { this.player = player; }}
-                        key={`player-${node.id}`}
-                        autoPlay
-                        loop={(node.type === 'decision')}
-                        className="screen-video"
-                    >
-                        <source
-                            src={`http://localhost:8080/mp4/${node.path}`}
-                        />
-                        <track
-                            label="English"
-                            kind="subtitles"
-                            srcLang="en"
-                            src={`http://localhost:8080/vtt/${node.path}`}
-                            default
-                        />
-                    </Video>
-                    {node.annotations &&
-                        <div className="location-description location-typing">
-                            <pre>{node.annotations.locationDesc}</pre>
-                        </div>
-                    }
-                    {node.type === 'decision' &&
-                        <div className="ui grid container equal width decision-container">
-                            <div className="column decision-item-container" />
-                            <div className="column decision-item-container" />
-                            {node.items.map(d => (
-                                <div
-                                    id="decision-container"
-                                    key={`${d.id}${d.type}`}
-                                    className="column decision-item-container"
-                                    onClick={() => this.handleOnClick(d)}
-                                    onMouseOver={() => { document.getElementById(d.type).play(); }}
-                                    onMouseOut={() => document.getElementById(d.type).pause()}
-                                    onFocus={() => {}}
-                                    onBlur={() => {}}
-                                >
-                                    <video
-                                        id={d.type}
-                                        loop
-                                        src={`http://localhost:8080/mp4/${d.path}`}
-                                        className="decision-video"
-                                    />
-                                    <p className="decision-item">{d.desc}</p>
-                                </div>
-                            ))}
-                            <div className="column decision-item-container" />
-                            <div className="column decision-item-container" />
-                        </div>
-                    }
-                </div>
+                <Video
+                    ref={(player) => { this.player = player; }}
+                    key={`player-${node.id}`}
+                    autoPlay
+                    loop={(node.type === 'decision')}
+                    className="screen-video"
+                    onEnded={() => this.handleOnEnded()}
+                    onClick={() => this.handleOnClick()}
+                    onDoubleClick={() => this.handleOnDoubleClick()}
+                >
+                    <source
+                        src={`http://localhost:8080/mp4/${node.path}`}
+                    />
+                    <track
+                        label="English"
+                        kind="subtitles"
+                        srcLang="en"
+                        src={`http://localhost:8080/vtt/${node.path}`}
+                        default
+                    />
+                </Video>
+                {node.annotations &&
+                    <div className="location-description location-typing">
+                        <pre>{node.annotations.locationDesc}</pre>
+                    </div>
+                }
+                {node.type === 'decision' &&
+                    <div className="ui grid container equal width decision-container">
+                        <div className="column decision-item-container" />
+                        <div className="column decision-item-container" />
+                        {node.items.map(d => (
+                            <div
+                                id="decision-container"
+                                key={`${d.id}${d.type}`}
+                                className="column decision-item-container"
+                                onClick={() => this.handleDecisionOnClick(d)}
+                                onMouseOver={() => { document.getElementById(d.type).play(); }}
+                                onMouseOut={() => document.getElementById(d.type).pause()}
+                                onFocus={() => {}}
+                                onBlur={() => {}}
+                            >
+                                <video
+                                    id={d.type}
+                                    loop
+                                    src={`http://localhost:8080/mp4/${d.path}`}
+                                    className="decision-video"
+                                />
+                                <p className="decision-item">{d.desc}</p>
+                            </div>
+                        ))}
+                        <div className="column decision-item-container" />
+                        <div className="column decision-item-container" />
+                    </div>
+                }
             </div>
         );
     }
