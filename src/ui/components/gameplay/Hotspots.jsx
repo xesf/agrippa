fdda36427b40d05741448ce2a180571d58c2fa94
Alexandre Fontoura <xesfnet@gmail.com>
2019-08-13 22:03:25 +0100

    Fixed hotspot position and styling

--- src/ui/components/gameplay/Hotspots.jsx
+++ src/ui/components/gameplay/Hotspots.jsx
@@ -7,26 +7,23 @@ const Hotspots = ({ items, onClick, editor }) => {
         }
     };
     return (
-        <div className="hotspot-container">
-            {items && items.map((h) => {
-                // bars offset
-                const stylePos = {
-                    position: 'absolute',
-                    left: h.x1,
-                    top: h.y1 + 100,
-                    width: h.x2 - h.x1,
-                    height: (h.y2 - h.y1),
-                    border: (editor) ? '1px dashed gray' : '',
-                };
-                return (<div
-                    id="hotspot-container"
-                    key={`${h.unk1}`}
-                    className="hotspot-item"
-                    style={stylePos}
-                    onClick={() => handleOnClick(h)}
-                />);
-            })}
-        </div>
+        items && items.map((h) => {
+            // bars offset
+            const stylePos = {
+                position: 'absolute',
+                left: h.x1 + 20,
+                top: h.y1 + 100,
+                width: h.x2 - h.x1,
+                height: (h.y2 - h.y1),
+                border: (editor) ? '1px dashed gray' : '',
+            };
+            return (<div
+                key={`${h.unk1}`}
+                className="hotspot-item"
+                style={stylePos}
+                onClick={() => handleOnClick(h)}
+            />);
+        })
     );
 };
 
